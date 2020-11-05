09c90a432845de783b71655eff709584eda4ecd8
Loïc Delmaire <loic.delmaire@gmail.com>
2020-11-05 16:38:43 +0100

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ window.addEventListener('load', function() {
 
   function init() {
     checkExternalLinks()
-  } 
+    setupSearch()
+  }
 
   function checkExternalLinks() {
     const links = document.querySelectorAll('a')
@@ -13,5 +14,58 @@ window.addEventListener('load', function() {
         links[i].classList.add('external-link')
       }
     }
+  }
+
+  function setupSearch() {
+    const searchResultsId = '#hits';
+    const searchBoxId = '#search-box';
+
+    var search = instantsearch({
+      appId: '4NUM23WKJI',
+      apiKey: 'b42036d3e76e8385accfb660836a59d1',
+      indexName: 'prod_api_entreprise_site',
+      routing: true,
+      searchFuntion: function(helper) {
+        var searchResults = $(searchResultsId);
+
+        console.log(helper.state.query);
+        if (helper.state.query === '') {
+          searchResults.hide();
+          return;
+        }
+        console.log('search');
+
+        helper.search();
+        searchResults.show();
+      }
+    });
+
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: searchBoxId,
+        placeholder: 'Rechercher',
+        cssClasses: {
+          root: 'algolia-search',
+        }
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+          empty: '<h5>' +
+              'Pas de résultat à votre recherche' +
+            '</h5>',
+          item: '<a class="" href="{{url}}">' +
+            '<div class="">' +
+              '{{{_highlightResult.name.value}}}' +
+            '</div>' +
+          '</a>'
+        }
+      })
+    );
+
+    search.start();
   }
 })
