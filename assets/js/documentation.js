839bbb6898feb10295c6e58c493cd1f372abb88f
Loïc Delmaire <loic.delmaire@gmail.com>
2021-01-27 11:46:42 +0100

    Automagically add anchors on each header in documentation

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -6,31 +6,60 @@ window.addEventListener('load', function (e) {
 
   const links = document.querySelectorAll('.documentation a')
 
+  addAnchorButtonsToDocumentContentPanel();
+
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', onTitleClick, false)
   }
-
-  function onTitleClick(evt) {
-    const href = evt.target.attributes.href.value
-    const index = href.indexOf('#')
-    if (index >= 0) {
-      openDetails(decodeURI(href.substring(index)))
-    }
-  }
-
-  function openDetails(hash) {
-    const target = document.getElementById(hash.substring(1))
-    if (target) {
-      let el = target
-
-      while (el.closest('details')) {
-        if (el.tagName.toLowerCase() === 'details') {
-          el.open = true
-        }
-        el = el.parentNode
-      }
-
-      target.scrollIntoView()
-    } 
-  }
 })
+
+function addAnchorButtonsToDocumentContentPanel() {
+  const panelsContentHeadings = document.querySelectorAll('.documentation .panel-content h6')
+
+  for (let i = 0; i < panelsContentHeadings.length; i++) {
+    buildAnchorButton(panelsContentHeadings[i]);
+  }
+}
+
+function buildAnchorButton(heading) {
+  var svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svgNode.setAttributeNS(null, 'viewBox', '0 0 24 24');
+  svgNode.setAttribute('class', 'tpl-icon');
+
+  var buttonNode = document.createElement('button');
+  buttonNode.setAttribute('class', 'tpl-button tpl-button--alternate button-link');
+  buttonNode.setAttribute('data-link', 'doc/#'+heading.id);
+  buttonNode.setAttribute('title', 'Copier le lien vers cette section');
+
+  buttonNode.appendChild(svgNode);
+  buttonNode.addEventListener('click', onButtonClick, false);
+  heading.appendChild(buttonNode);
+
+  var useSvg = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  useSvg.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#link');
+  svgNode.appendChild(useSvg);
+}
+
+function onTitleClick(evt) {
+  const href = evt.target.attributes.href.value
+  const index = href.indexOf('#')
+  if (index >= 0) {
+    openDetails(decodeURI(href.substring(index)))
+  }
+}
+
+function openDetails(hash) {
+  const target = document.getElementById(hash.substring(1))
+  if (target) {
+    let el = target
+
+    while (el.closest('details')) {
+      if (el.tagName.toLowerCase() === 'details') {
+        el.open = true
+      }
+      el = el.parentNode
+    }
+
+    target.scrollIntoView()
+  }
+}
--- assets/js/links.js
+++ assets/js/links.js
@@ -4,70 +4,70 @@ window.addEventListener('load', function() {
   for (let j = 0; j < linkButtons.length; j++) {
     linkButtons[j].addEventListener('click', onButtonClick, false)
   }
-
-  function getFullHostUrl() {
-    const host = window.location.host;
-    const hostname = window.location.hostname;
-
-    if (hostname === 'entreprise.api.gouv.fr') {
-      return `https://${host}`;
-    }
-    else if (hostname === 'localhost') {
-      return `${host}/entreprise.api.gouv.fr`;
-    }
-    else {
-      return `http://${host}/entreprise.api.gouv.fr`;
-    }
-  }
-
-  function onButtonClick(evt) {
-    const target = evt.target
-    if (target) {
-      const link = `${getFullHostUrl()}/${target.getAttribute('data-link')}`
-
-      if (link) {
-        copyTextToClipboard(link)
-      }
-    }
-  }
-
-  function copyTextToClipboard(text) {
-    if (!navigator.clipboard) {
-        fallbackCopyTextToClipboard(text)
-        return
-    }
-    navigator.clipboard.writeText(text).then(function() {
-        const flash = document.getElementById('flash')
-        flash.innerHTML = 'Le lien a bien été copié'
-        flash.style.display = 'block'
-        setTimeout(() => {
-        flash.style.display = 'none'
-        }, 2500)
-    }, function(err) {
-        console.error('Le texte n’a pas pu être copié : ', err)
-    })
-  }
-
-  function fallbackCopyTextToClipboard(text) {
-    let textArea = document.createElement("textarea")
-    textArea.value = text
-
-    // Avoid scrolling to bottom
-    textArea.style.top = "0"
-    textArea.style.left = "0"
-    textArea.style.position = "fixed"
-
-    document.body.appendChild(textArea)
-    textArea.focus()
-    textArea.select()
-
-    try {
-        const successful = document.execCommand('copy')
-        const msg = successful ? 'successful' : 'unsuccessful'
-    } catch (err) {
-        console.error('Le texte n’a pas pu être copié : ', err)
-    }
-
-    document.body.removeChild(textArea)
-  }
 })
+
+function getFullHostUrl() {
+  const host = window.location.host;
+  const hostname = window.location.hostname;
+
+  if (hostname === 'entreprise.api.gouv.fr') {
+    return `https://${host}`;
+  }
+  else if (hostname === 'localhost') {
+    return `${host}/entreprise.api.gouv.fr`;
+  }
+  else {
+    return `http://${host}/entreprise.api.gouv.fr`;
+  }
+}
+
+function onButtonClick(evt) {
+  const target = evt.target
+  if (target) {
+    const link = `${getFullHostUrl()}/${target.getAttribute('data-link')}`
+
+    if (link) {
+      copyTextToClipboard(link)
+    }
+  }
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text)
+      return
+  }
+  navigator.clipboard.writeText(text).then(function() {
+      const flash = document.getElementById('flash')
+      flash.innerHTML = 'Le lien a bien été copié'
+      flash.style.display = 'block'
+      setTimeout(() => {
+      flash.style.display = 'none'
+      }, 2500)
+  }, function(err) {
+      console.error('Le texte n’a pas pu être copié : ', err)
+  })
+}
+
+function fallbackCopyTextToClipboard(text) {
+  let textArea = document.createElement("textarea")
+  textArea.value = text
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0"
+  textArea.style.left = "0"
+  textArea.style.position = "fixed"
+
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
+
+  try {
+      const successful = document.execCommand('copy')
+      const msg = successful ? 'successful' : 'unsuccessful'
+  } catch (err) {
+      console.error('Le texte n’a pas pu être copié : ', err)
+  }
+
+  document.body.removeChild(textArea)
+}
