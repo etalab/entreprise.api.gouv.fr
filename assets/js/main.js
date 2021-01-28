94c68a69ba648330eb6b622460571b50f1ed1058
Loïc Delmaire <loic.delmaire@gmail.com>
2021-01-28 13:46:31 +0100

    Add algolia for support

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ window.addEventListener('load', function() {
 
   function init() {
     checkExternalLinks()
-  } 
+    setupSearch()
+  }
 
   function checkExternalLinks() {
     const links = document.querySelectorAll('a')
@@ -13,5 +14,62 @@ window.addEventListener('load', function() {
         links[i].classList.add('external-link')
       }
     }
+  }
+
+  function setupSearch() {
+    const searchResultsId = '#hits';
+    const searchBoxId = '#search-box';
+
+    var searchClient = algoliasearch(
+      '4NUM23WKJI',
+      'b42036d3e76e8385accfb660836a59d1'
+    );
+    var search = instantsearch({
+      indexName: 'prod_api_entreprise_site',
+      searchClient
+    });
+
+    // https://www.algolia.com/doc/api-reference/api-parameters/filters/
+    search.addWidget(
+      instantsearch.widgets.configure({
+        filters: 'enable:true AND kind:support'
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: searchBoxId,
+        placeholder: 'Tapez votre question ici',
+        cssClasses: {
+          root: 'algolia-search',
+        }
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.hits({
+        container: '#hits',
+        escapeHTML: false,
+        templates: {
+          empty: '',
+          item(result) {
+            return `
+              <details class="entry fold" id="${ result.objectID }">
+                <summary>
+                  <h3>${ result.question }</h3>
+                  <span class="label">
+                    ${ result.label }
+                  </span>
+                </summary>
+                <div class="panel-content">
+                 ${ result.answer }
+                </div>
+            `;
+          }
+        }
+      })
+    );
+
+    search.start();
   }
 })
