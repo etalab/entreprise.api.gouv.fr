7ecc9f7a8db36bdfad57e24163235abc0a601116
Loïc Delmaire <loic.delmaire@gmail.com>
2021-03-02 16:17:42 +0100

    Introduce scroll to top for mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function() {
 
   function init() {
     checkExternalLinks()
+    initScrollToTop()
   }
 
   function checkExternalLinks() {
@@ -11,6 +12,21 @@ window.addEventListener('load', function() {
     for(let i = 0; i < links.length; i++) {
       if (links[i].hasAttribute('target') && links[i].getAttribute('target') === '_blank') {
         links[i].classList.add('external-link')
+      }
+    }
+  }
+
+  function initScrollToTop() {
+    var scrollToTopButton = document.getElementById('scroll-to-top');
+
+    window.onscroll = function() {
+      var offset = 20;
+
+      if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+        scrollToTopButton.classList.remove('scroll-hidden');
+      }
+      else {
+        scrollToTopButton.classList.add('scroll-hidden');
       }
     }
   }
